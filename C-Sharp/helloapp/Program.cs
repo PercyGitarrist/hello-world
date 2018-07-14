a9b51fc70f22aa0068a8b49a64c97aa3292a804b
PercyGitarrist <percyj1411@gmail.com>
2018-07-14 11:27:20 +0200

    Encapsulation of July 14th

--- C-Sharp/helloapp/Program.cs
+++ C-Sharp/helloapp/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
-namespace DeclaringConstants {
-    class Program {
-        static void Main(string[] args) {
-            const double pi =3.14159;
-            
-            //constant declaration
-            double r;
-            Console.WriteLine("Enter Radius: ");
-            r = Convert.ToDouble(Console.ReadLine());
 
-            double areaCricle = pi * r * r;
-            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCricle);
-            Console.ReadLine();
-        }
-    }
+namespace RectangleApplication {
+
+   class Rectangle {
+      //member variables
+      private double length;
+      private double width;
+      
+      public void Acceptdetails() {
+         Console.WriteLine("Enter Length: ");
+         length = Convert.ToDouble(Console.ReadLine());
+         Console.WriteLine("Enter Width: ");
+         width = Convert.ToDouble(Console.ReadLine());
+      }
+      
+      public double GetArea() {
+         return length * width;
+      }
+      
+      public void Display() {
+         Console.WriteLine("Length: {0}", length);
+         Console.WriteLine("Width: {0}", width);
+         Console.WriteLine("Area: {0}", GetArea());
+      }
+   }//end class Rectangle
+   
+   class ExecuteRectangle {
+      static void Main(string[] args) {
+         Rectangle r = new Rectangle();
+         r.Acceptdetails();
+         r.GetArea();
+         r.Display();
+         Console.ReadLine();
+      }
+   }
 }
